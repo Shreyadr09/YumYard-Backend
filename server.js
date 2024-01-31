201a8934650b48625d8f770a48f5f1bb4bbbca82
Shreya DR <shreyadr09@gmail.com>
2024-01-31 14:41:22 +0530

    doceker compose added

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 dotenv.config();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 connectDB();
 
 const PORT = process.env.PORT || 5000;
@@ -22,7 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, "dist")));
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
